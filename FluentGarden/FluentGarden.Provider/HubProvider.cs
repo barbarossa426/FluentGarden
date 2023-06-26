d0a524d0d46f24a8b19a2b2a3559992b9e10605a
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-06-26 09:42:27 +0200

    feat(fluentGarden) privdider added

---
+++ FluentGarden/FluentGarden.Provider/HubProvider.cs
@@ -0,0 +1,47 @@
+﻿using FluentGarden.Provider.Interfaces;
+using FluentGarden.Repository.Models;
+
+namespace FluentGarden.Provider;
+
+public class HubProvider : IHubProvider
+{
+    public Task AddDeviceToHub(Device device)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CheckIn(string ip)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Device> GetDeviceByIp(string ip)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<DateTime> GetHubTime()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Device>> ListDevices()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Ping(string ip)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> RemoveDeviceFromHub(Device device)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task TriggerDevice(Device device, params DeviceAction[] actions)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FluentGarden/FluentGarden.Provider/Interfaces/IHubProvider.cs
@@ -0,0 +1,22 @@
+﻿using FluentGarden.Repository.Models;
+
+namespace FluentGarden.Provider.Interfaces;
+
+public interface IHubProvider
+{
+    Task AddDeviceToHub(Device device);
+
+    Task<int> RemoveDeviceFromHub(Device device);
+
+    Task TriggerDevice(Device device, params DeviceAction[] actions);
+
+    Task<List<Device>> ListDevices();
+
+    Task<Device> GetDeviceByIp(string ip);
+
+    Task<bool> Ping(string ip);
+
+    Task<DateTime> GetHubTime();
+
+    Task CheckIn(string ip);
+}
