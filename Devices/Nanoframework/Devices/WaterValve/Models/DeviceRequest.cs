a030ca7a61ccdfc4f0bdf397802d708c29c69390
Peter L <61692826+PeterLAxakon@users.noreply.github.com>
2023-06-28 17:04:20 +0200

    feat(networkService) added support for coinnection to hub using http request

---
+++ Devices/Nanoframework/Devices/WaterValve/Models/DeviceRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WaterValve.Models
+{
+    public class DeviceRequest
+    {
+        public string MacAddress { get; set; }
+        public string Type { get; set; } = "esp32";
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Devices/Nanoframework/Devices/WaterValve/NetworkService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using WaterValve.Models;
+
+namespace WaterValve
+{
+    public class NetworkService : INetworkService
+    {
+        public string BaseRoute { get; set; }
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public string ConnectToHub(DeviceRequest request)
+        {
+            try
+            {
+                string endpoint = $"{BaseRoute}/devices";
+
+                string requestAsjson = nanoFramework.Json.JsonSerializer.SerializeObject(request);
+
+                var content = new StringContent(requestAsjson, Encoding.UTF8, "application/json");
+                var result = _httpClient.Post("https://httpbin.org/anything", content);
+                result.EnsureSuccessStatusCode();
+
+                var output = "";
+                return output;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+
+    public interface INetworkService
+    {
+        string ConnectToHub(DeviceRequest request);
+    }
+}
